f1bf9b4a055888b89204af642679c410360b7a02
Md Zeya Deoraji <148754120+Zeyadeoraji@users.noreply.github.com>
2023-12-12 15:44:44 +0530

    Updated Main.js

--- Main.js
+++ Main.js
@@ -11,6 +11,7 @@ arr.forEach((buttons) => {
     // console.log(buttons);
     if (e.target.innerHTML == "=") {
       display.value = eval(string);
+      string = "";
       // console.log(display.value);
       display.value.innerHTML = string;
     } else if (e.target.innerHTML == "AC") {
